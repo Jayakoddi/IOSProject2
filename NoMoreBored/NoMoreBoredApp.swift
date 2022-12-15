79a27453bf7f044c3f2357a23f31266116907a76
Jayakoddi <priyanathan.christy@gmail.com>
2022-12-15 13:32:49 +0100

    Initial Commit

---
+++ NoMoreBored/NoMoreBoredApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoMoreBoredApp.swift
+//  NoMoreBored
+//
+//  Created by Christy Dinakaran on 2022-12-15.
+//
+
+import SwiftUI
+
+@main
+struct NoMoreBoredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
